b82cf974debf579821997dc239759a326bcafd82
Kipleting <kipletingedwin4@gmail.com>
2022-09-11 12:58:18 +0300

    remove some redundant code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,8 @@
-# require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  # get "/" do
-  #   { message: "Good luck with your project!" }.to_json
-  # end
+ 
 
   get '/details' do
    all_users=Detail.all
