5949a4973b271542b5944f2c5bc6031c088671cc
Kipleting <kipletingedwin4@gmail.com>
2022-09-12 17:59:43 +0300

    delete unwanted code

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,17 +14,17 @@ Detail.create(country:"United States of America", town:"Los Angeles",date:2021, 
 Detail.create(country:"South Africa", town:"Cape Town",date:2022, description:"Cape Town is the best place to be man,")
 Detail.create(country:"Australia", town:"Perth",date:2022, description:"Perth is the best place to be man,")
 
-Login.create(email:"edwin@gmail.com",password:"12345")
-Login.create(email:"peter@gmail.com",password:"67890")
-Login.create(email:"james@gmail.com",password:"111213")
-Login.create(email:"jack@gmail.com",password:"141516")
-Login.create(email:"hendo@gmail.com",password:"181920")
+# Login.create(email:"edwin@gmail.com",password:"12345")
+# Login.create(email:"peter@gmail.com",password:"67890")
+# Login.create(email:"james@gmail.com",password:"111213")
+# Login.create(email:"jack@gmail.com",password:"141516")
+# Login.create(email:"hendo@gmail.com",password:"181920")
 
-Register.create(username:"edwin",email:"edwin@gmail.com",password:"12345")
-Register.create(username:"peter",email:"peter@gmail.com",password:"12345")
-Register.create(username:"james",email:"james@gmail.com",password:"12345")
-Register.create(username:"jack",email:"jack@gmail.com",password:"12345")
-Register.create(username:"hendo",email:"hendo@gmail.com",password:"12345")
+# Register.create(username:"edwin",email:"edwin@gmail.com",password:"12345")
+# Register.create(username:"peter",email:"peter@gmail.com",password:"12345")
+# Register.create(username:"james",email:"james@gmail.com",password:"12345")
+# Register.create(username:"jack",email:"jack@gmail.com",password:"12345")
+# Register.create(username:"hendo",email:"hendo@gmail.com",password:"12345")
 
 
 
